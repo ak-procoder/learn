3ac837523e2d6e4300a2a313fada9e866afa29e3
ak-procoder <akuselan.career@gmail.com>
2025-09-19 14:39:10 +0000

    Build: (99b0d05) MVP Push

---
+++ _next/static/chunks/1ac68445df9cc873.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/5bce4476fa00258d.js",
+  "static/chunks/turbopack-7ba92a96da124f42.js"
+])
---
+++ _next/static/chunks/9791fa16010b2970.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/5bce4476fa00258d.js",
+  "static/chunks/turbopack-a14c10e7c466a65c.js"
+])
