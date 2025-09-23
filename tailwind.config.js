7632b648ec9cf5e58f68ad60a9c413bcbe0e98ee
ak-procoder <ak.procoder@gmail.com>
2025-09-23 17:03:44 -0400

    Added media break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '640px',   // Small devices (large phones, 640px and up)
+        'md': '768px',   // Medium devices (tablets, 768px and up)  
+        'lg': '1024px',  // Large devices (laptops, 1024px and up)
+        'xl': '1280px',  // Extra large devices (large laptops, 1280px and up)
+        '2xl': '1536px', // 2X Extra large devices (large desktops, 1536px and up)
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
